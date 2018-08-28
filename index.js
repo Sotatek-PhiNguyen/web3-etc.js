f97877d1cb767c607ace3994c313b9e60a5c5235
Sotatek-PhiNguyen <phi.nguyen@sotatek.com>
2018-08-28 17:07:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var Web3_etc = require('./lib/web3');
 
 // dont override global variable
-if (typeof window !== 'undefined' && typeof window.Web3 === 'undefined') {
-    window.Web3 = Web3_etc;
+if (typeof window !== 'undefined' && typeof window.Web3_etc === 'undefined') {
+    window.Web3_etc = Web3_etc;
 }
 
 module.exports = Web3_etc;
