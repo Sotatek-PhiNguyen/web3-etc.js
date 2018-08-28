a2130a3cbe3064ac56c0472ec9d848dc7bc398c2
Sotatek-PhiNguyen <phi.nguyen@sotatek.com>
2018-08-28 17:23:07 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var Web3_etc = require('./lib/web3-etc');
-
-// dont override global variable
-if (typeof window !== 'undefined' && typeof window.Web3_etc === 'undefined') {
-    window.Web3_etc = Web3_etc;
-}
-
-module.exports = Web3_etc;
